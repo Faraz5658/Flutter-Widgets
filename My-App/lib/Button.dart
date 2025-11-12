367cd2cc6fbef739f14ccb1177d068bbefa8ce76
Faraz5658 <farazahmad5623@gmail.com>
2025-11-12 07:09:14 +0500

    Button add in My App

---
+++ My-App/lib/Button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: const MyHomePage(title: 'Flutter Image Asset Example'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text("THIS IS CENTER WIDGET"),
+      ),
+      body: OutlinedButton(
+        child: Text("Click Here..."),
+        onPressed: () {
+          print("OutlinedButton Clicked...");
+        },
+        onLongPress: () {
+          print("OutlinedButton Long Pressed...");
+        },
+      ),
+    );
+  }
+}
