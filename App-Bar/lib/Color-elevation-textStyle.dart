b07e130d195e9ed76af4b50c43c8e2c4bc16bd3e
Faraz5658 <farazahmad5623@gmail.com>
2025-11-13 14:10:56 +0500

    Color-Elevation-TextStyle

---
+++ App-Bar/lib/Color-elevation-textStyle.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Styling: color, elevation, text style...',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.black,
+          elevation: 10.0,
+          toolbarHeight: 70.0,
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text(
+            'Color, Elevation, Text Style!',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
