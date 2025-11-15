57307f1277292b14f670f1f4609c234b2a2a4605
Faraz5658 <farazahmad5623@gmail.com>
2025-11-15 09:17:31 +0500

    Different ListLiew

---
+++ My-App/lib/ListLiew.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Icon Page',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: const MyHomePage(title: 'Icon Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text("Icon Page"),
+      ),
+      body:
+          // ListView Simple
+          // ListView(
+          //   children: [
+          //     Text("ONE"),
+          //     Text("TWO"),
+          //     Text("THREE"),
+          //     Text("FOUR"),
+          //     Text("FIVE"),
+          //     Text("SIX"),
+          //   ],
+          // ),
+          //ListView Builder
+          // ListView.builder(
+          //   itemCount: 7,
+          //   itemBuilder: (context, index) {
+          //     return Text("Item $index");
+          //   },
+          // ),
+          ListView.separated(
+            itemCount: 7,
+            separatorBuilder: (context, index) => Divider(),
+            itemBuilder: (context, index) {
+              return Text('TEXT NO $index');
+            },
+          ),
+    );
+  }
+}
