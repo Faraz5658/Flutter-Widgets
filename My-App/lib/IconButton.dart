bbefa431265e253ad1de361581b79533a1dfb477
Faraz5658 <farazahmad5623@gmail.com>
2025-11-15 09:00:39 +0500

    IconButton

---
+++ My-App/lib/IconButton.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Icon Page',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: const MyHomePage(title: 'Icon Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text("Icon Page"),
+      ),
+      body: IconButton(
+        icon: Icon(Icons.info),
+        tooltip: 'More Info',
+        onPressed: () {
+          print('INFO BUTTON PRESS');
+        },
+      ),
+    );
+  }
+}
