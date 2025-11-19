9c5c387f0f825e5ce5dafb9da0dd385f3d9bb0cf
Faraz5658 <farazahmad5623@gmail.com>
2025-11-19 21:11:43 +0500

    flexible

---
+++ My-App/lib/flexible.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: const MyHomePage(title: 'Flutter Row Flexible Example'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text("THIS IS ROW WITH Flexible WIDGET"),
+      ),
+      body: Row(
+        children: [
+          Container(color: Colors.amberAccent, width: 500, height: 50),
+          Flexible(child: Container(height: 50, color: Colors.red)),
+        ],
+      ),
+    );
+  }
+}
